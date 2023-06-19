4374c15c5e9fa16aefa3e3b21314724993685961
leo <leo.henches@gmail.com>
2023-06-19 16:42:44 +0200

    first set complete!

--- set1/src/main.rs
+++ set1/src/main.rs
@@ -1,49 +1,19 @@
+use openssl::cipher::Cipher;
+use openssl::cipher_ctx::CipherCtx;
 use std::collections::HashMap;
 use std::fs;
-use std::iter::once;
 
 fn main() {
-    // let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    // let hex = "49276d";
-    // let b64_encoding = hex_to_b64(&hex);
-    // dbg!(b64_encoding);
-    // let hex1 = "1c0111001f010100061a024b53535009181c".to_string();
-    // let hex2 = "686974207468652062756c6c277320657965".to_string();
-    // let xor_str = fixed_xor(&hex1, &hex2);
-    // dbg!(xor_str);
-    // ex 4
-    // let file_contents = fs::read_to_string("data/set_of_potential_xor.txt").unwrap();
-    // let min_all = file_contents
-    //     .split("\n")
-    //     .map(|test_string| get_min_message_decode(test_string, &char_freq))
-    //     .min_by(|t1, t2| t1.1.total_cmp(&t2.1))
-    //     .unwrap();
-    // dbg!(min_all);
-    // ex 5
-    let message =
-        "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal".to_string();
-    let key = "ICE".to_string();
-    let message_bytes: Vec<u8> = message.chars().map(|c| c as u8).collect();
-    let bytes_encrypted = encrypt_message_repeating_xor(&message_bytes, &key);
-    let hex_encrypted = byte_vec_to_hex_chars(bytes_encrypted.clone());
-    dbg!(hex_encrypted);
-    // ex 6
-    // 2: hamming distance
-    let hamming_test = hamming_distance("this is a test", "wokka wokka!!!");
-    dbg!(hamming_test);
-    // 3
-    let file_contents = fs::read_to_string("data/encrypted_message_repeating_xor.txt").unwrap();
-    let contents = &file_contents.replace("\n", "");
-    let bytes = b64_to_bytes(&contents);
-    // let results = decrypt_repeating_xor(&bytes);
-    let results = decrypt_repeating_xor(&bytes_encrypted);
-    dbg!(results);
-    // let hex_encrypted = byte_vec_to_hex_chars(bytes_encrypted);
+    // ex1();
+    // ex2();
+    // ex4();
+    // ex5();
+    // ex6();
+    // ex7();
+    ex8();
 }
 
 fn decrypt_repeating_xor(bytes: &Vec<u8>) -> String {
-    let char_freq = instanciate_hash_frequency();
-    // dbg!(min_hamming);
     let keysize_candidate = get_min_hamming(&bytes);
     let vec_blocks: Vec<String> = (0..keysize_candidate)
         .map(|skip| {
@@ -59,46 +29,36 @@ fn decrypt_repeating_xor(bytes: &Vec<u8>) -> String {
         })
         .collect();
 
-    // dbg!(vec_blocks[0].clone());
-    // dbg!(vec_blocks[1].clone());
+    let char_freq = instanciate_hash_frequency();
     let keys: Vec<_> = vec_blocks
         .into_iter()
         .map(|str_coded| get_min_message_decode(&str_coded, &char_freq).2)
         .collect();
-    // dbg!(keys.clone());
     let string_keys: String = keys.into_iter().map(|k| k as char).collect();
-    // dbg!(string_keys.clone());
     let bytes_decoded = encrypt_message_repeating_xor(&bytes, &string_keys);
     let string_decoded: String = bytes_decoded.into_iter().map(|b| b as char).collect();
     string_decoded
 }
 
 fn get_min_hamming(bytes: &Vec<u8>) -> usize {
-    // let vec_hamming_with_size: Vec<_> = (1..40)
-    let vec_hamming_with_size: Vec<_> = (3..=3)
+    // let vec_hamming_with_size: Vec<_> = (2..=4)
+    let vec_hamming_with_size: Vec<_> = (1..40)
         .map(|keysize| {
+            let max_chunk_bytes = bytes.len() / keysize;
             let mut iter_bytes = bytes.iter();
             let mut_ref_iter = iter_bytes.by_ref();
             let bytes_first: Vec<u8> = mut_ref_iter.take(keysize).cloned().collect();
-            let bytes_second: Vec<u8> = mut_ref_iter.take(keysize).cloned().collect();
-            let bytes_third: Vec<u8> = mut_ref_iter.take(keysize).cloned().collect();
-            let bytes_forth: Vec<u8> = mut_ref_iter.take(keysize).cloned().collect();
-            // dbg!(bytes_first.clone());
-            // dbg!(bytes_second.clone());
-            let hamming_1 = hamming_distance_bytes(&bytes_first, &bytes_second);
-            let hamming_2 = hamming_distance_bytes(&bytes_second, &bytes_third);
-            let hamming_3 = hamming_distance_bytes(&bytes_third, &bytes_forth);
-            let hamming_4 = hamming_distance_bytes(&bytes_first, &bytes_forth);
-            let normalized_hamming_1 = hamming_1 as f64 / keysize as f64;
-            let normalized_hamming_2 = hamming_2 as f64 / keysize as f64;
-            let normalized_hamming_3 = hamming_3 as f64 / keysize as f64;
-            let normalized_hamming_4 = hamming_4 as f64 / keysize as f64;
-            let normalized_hamming = normalized_hamming_1
-                + normalized_hamming_2
-                + normalized_hamming_3
-                + normalized_hamming_4;
-            dbg!(normalized_hamming);
-            (keysize, normalized_hamming)
+            let hamming_candidate: f64 = (1..max_chunk_bytes)
+                .map(|_| {
+                    let bytes_current: Vec<u8> = mut_ref_iter.take(keysize).cloned().collect();
+                    let hamming = hamming_distance_bytes(&bytes_first, &bytes_current);
+                    let normalized_hamming =
+                        hamming as f64 / (keysize as f64 * (max_chunk_bytes - 1) as f64);
+                    normalized_hamming
+                })
+                .sum();
+            // let sum_hamming = hamming_1 + hamming_2;
+            (keysize, hamming_candidate)
         })
         .collect();
     vec_hamming_with_size
@@ -112,7 +72,7 @@ fn c_str_to_bytes(c_str: &str) -> Vec<u8> {
     c_str.chars().map(|c| c as u8).collect()
 }
 
-fn hamming_distance_bytes(bytes_first: &Vec<u8>, bytes_second: &Vec<u8>) -> u32 {
+fn hamming_distance_bytes(bytes_first: &[u8], bytes_second: &[u8]) -> u32 {
     bytes_first
         .into_iter()
         .zip(bytes_second.into_iter())
@@ -152,15 +112,11 @@ fn encrypt_message_repeating_xor(message: &Vec<u8>, key: &str) -> Vec<u8> {
 fn get_min_message_decode(test_string: &str, char_freq: &HashMap<char, f64>) -> (String, f64, u8) {
     let message_and_scores: Vec<_> = (0..=255)
         .map(|byte_key| {
-            let (c1, c2) = convert_u8_to_hex_tuple(byte_key);
-            let hex_str_decode: String = once(c1)
-                .chain(once(c2))
-                .cycle()
-                .take(test_string.len())
+            let key_repeat = std::iter::repeat(byte_key)
+                .take(test_string.len() / 2)
                 .collect();
-            // dbg!(hex_str_decode.clone());
-            let decoded_hex = fixed_xor(&test_string, &hex_str_decode);
-            let bytes_decoded = hex_string_to_byte_vec(&decoded_hex);
+            let test_bytes = hex_string_to_byte_vec(test_string);
+            let bytes_decoded = fixed_xor(test_bytes, key_repeat);
             let message: String = bytes_decoded.into_iter().map(|b| b as char).collect();
             let score = score_string(&message, &char_freq);
             (message, score, byte_key)
@@ -173,18 +129,15 @@ fn get_min_message_decode(test_string: &str, char_freq: &HashMap<char, f64>) -> 
 }
 
 fn hex_to_b64(hex: &str) -> String {
-    let bytes: Vec<u32> = hex
-        .chars()
-        .map(|c| convert_hex_char_to_u8(c) as u32)
-        .collect();
-    let b64: String = bytes
-        .chunks(6)
+    let bytes = hex_string_to_byte_vec(hex);
+    bytes
+        .chunks(3)
         .flat_map(|chunk| {
-            let mut num = 0;
+            let mut num: u32 = 0;
             let mut current;
-            for index in 0..=5 {
-                current = chunk.get(index).cloned().unwrap_or(0);
-                num += current << ((20 - index * 4) as u32);
+            for index in 0..=2 {
+                current = chunk[index] as u32;
+                num += current << ((16 - index * 8) as u32);
             }
             // we have 24 bits filled
             // now we convert them 6 bits at a time
@@ -198,8 +151,7 @@ fn hex_to_b64(hex: &str) -> String {
             }
             string_results.chars().collect::<Vec<_>>().into_iter()
         })
-        .collect();
-    b64
+        .collect()
 }
 
 fn b64_to_bytes(b64: &str) -> Vec<u8> {
@@ -220,7 +172,7 @@ fn b64_to_bytes(b64: &str) -> Vec<u8> {
                 }
             }
             let mask = 2u32.pow(8) - 1;
-            let bytes: Vec<_> = (0..=cur_num_bytes_decode)
+            let bytes: Vec<_> = (0..cur_num_bytes_decode)
                 .map(|byte_i| {
                     let byte = result >> ((2 - byte_i) * 8) & mask;
                     byte as u8
@@ -232,7 +184,6 @@ fn b64_to_bytes(b64: &str) -> Vec<u8> {
 }
 
 fn letter_to_6_bits(letter: char) -> u8 {
-    // dbg!(letter.clone());
     match letter {
         'A'..='Z' => letter as u8 - 'A' as u8,
         'a'..='z' => letter as u8 - 'a' as u8 + 26u8,
@@ -265,15 +216,18 @@ fn byte_vec_to_hex_chars(bytes: Vec<u8>) -> String {
         .collect()
 }
 
-fn fixed_xor(hex1: &str, hex2: &str) -> String {
-    let bytes_1 = hex_string_to_byte_vec(hex1);
-    let bytes_2 = hex_string_to_byte_vec(hex2);
-    let vec_xor: Vec<_> = bytes_1
+fn fixed_xor(bytes_1: Vec<u8>, bytes_2: Vec<u8>) -> Vec<u8> {
+    bytes_1
         .into_iter()
         .zip(bytes_2.into_iter())
         .map(|(b1, b2)| b1 ^ b2)
-        .collect();
-    // dbg!(vec_xor.clone());
+        .collect()
+}
+
+fn fixed_xor_hex(hex1: &str, hex2: &str) -> String {
+    let bytes_1 = hex_string_to_byte_vec(hex1);
+    let bytes_2 = hex_string_to_byte_vec(hex2);
+    let vec_xor = fixed_xor(bytes_1, bytes_2);
     byte_vec_to_hex_chars(vec_xor)
 }
 
@@ -360,4 +314,91 @@ fn score_string(decoded: &str, char_freq: &HashMap<char, f64>) -> f64 {
         .sum::<f64>()
         / decoded.len() as f64;
     score + sum_alpha
+}
+fn ex1() {
+    let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    // let hex = "49276d";
+    let b64_encoding = hex_to_b64(&hex);
+    dbg!(b64_encoding);
+}
+fn ex2() {
+    let hex1 = "1c0111001f010100061a024b53535009181c".to_string();
+    let hex2 = "686974207468652062756c6c277320657965".to_string();
+    let xor_str = fixed_xor_hex(&hex1, &hex2);
+    dbg!(xor_str);
+}
+fn ex4() {
+    let char_freq = instanciate_hash_frequency();
+    let file_contents = fs::read_to_string("data/set_of_potential_xor.txt").unwrap();
+    let min_all = file_contents
+        .trim_matches('\n')
+        .split("\n")
+        .map(|test_string| {
+            let min_msg = get_min_message_decode(test_string, &char_freq);
+            min_msg
+        })
+        .min_by(|t1, t2| t1.1.total_cmp(&t2.1))
+        .unwrap();
+    dbg!(min_all);
+}
+fn ex5() {
+    let message =
+        "Burning 'em, if you ain't quick and nimble I go crazy when I hear a cymbal".to_string();
+    let key = "ICE".to_string();
+    let message_bytes: Vec<u8> = c_str_to_bytes(&message);
+    let bytes_encrypted = encrypt_message_repeating_xor(&message_bytes, &key);
+    let hex_encrypted = byte_vec_to_hex_chars(bytes_encrypted.clone());
+    dbg!(hex_encrypted);
+}
+fn ex6() {
+    let hamming_test = hamming_distance("this is a test", "wokka wokka!!!");
+    dbg!(hamming_test);
+    // // 3
+    let file_contents = fs::read_to_string("data/encrypted_message_repeating_xor.txt").unwrap();
+    let contents = &file_contents.replace("\n", "");
+    let bytes = b64_to_bytes(&contents);
+    let results = decrypt_repeating_xor(&bytes);
+    dbg!(results);
+}
+fn ex7() {
+    let key_string = "YELLOW SUBMARINE".to_string();
+    let key = c_str_to_bytes(&key_string);
+    let file_contents = fs::read_to_string("data/encrypted_ECB_AES-128.txt").unwrap();
+    let contents = &file_contents.replace("\n", "");
+    let bytes = b64_to_bytes(&contents);
+    let cipher = Cipher::aes_128_ecb();
+    let mut ctx = CipherCtx::new().unwrap();
+    ctx.decrypt_init(Some(cipher), Some(&key), None).unwrap();
+    let mut plaintext = vec![];
+    ctx.cipher_update_vec(&bytes, &mut plaintext).unwrap();
+    ctx.cipher_final_vec(&mut plaintext).unwrap();
+    let message: String = plaintext.into_iter().map(|b| b as char).collect();
+    dbg!(message);
+}
+
+fn ex8() {
+    let file_contents = fs::read_to_string("data/encrypted_ecb_candidates.txt").unwrap();
+    let min_all = file_contents
+        .trim_matches('\n')
+        .split("\n")
+        .enumerate()
+        .map(|(index, test_string)| {
+            // we count the number of redundancies between blocks of 16 bytes
+            let bytes_candidates = hex_string_to_byte_vec(test_string);
+            let sum_hammings: u32 = bytes_candidates
+                .windows(32)
+                .step_by(16)
+                .map(|slice| {
+                    let set_1 = &slice[0..16];
+                    let set_2 = &slice[16..32];
+                    let hamming = hamming_distance_bytes(set_1, set_2);
+                    hamming
+                })
+                .sum();
+            dbg!(sum_hammings);
+            (index, sum_hammings)
+        })
+        .min_by(|t1, t2| t1.1.cmp(&t2.1))
+        .unwrap();
+    dbg!(min_all);
 }
